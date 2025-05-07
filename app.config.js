072076c8a48e299b247d20dfd940f5bd7ccaaad2
Harfa Sabri <sabriharfa@gmail.com>
2025-05-07 19:05:55 +0700

    fix update

--- app.config.js
+++ app.config.js
@@ -23,10 +23,14 @@ export default {
       }
     },
     runtimeVersion: {
-      policy: "appVersion" // <- Gunakan ini agar tidak error di cloud
+      policy: "appVersion" // Menggunakan policy berdasarkan versi aplikasi
     },
     updates: {
-      enabled: true
+      enabled: true,
+      url: "https://u.expo.dev/aa15121b-0722-434f-90c7-04478c450b84", // URL pembaruan
+      channelName: "default", // Channel yang digunakan untuk pembaruan
+      platform: ["android"] // Platform yang didukung
     }
   }
 };
+
