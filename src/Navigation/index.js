862d11d83012e9bcca47406c19a76eee1eb97649
Harfa Sabri <sabriharfa@gmail.com>
2025-05-19 20:17:28 +0700

    fix

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -7,28 +7,17 @@ const RootNavigator = () => {
   const { 
     authUser, 
     loading, 
-     
-     
     resettingPassword 
   } = useAuthContext();
-
   if (loading) {
     return <SplashScreen />;
   }
-
-   
   if (resettingPassword) {
-     
     return <AuthStack />;
   }
-
-   
   if (!authUser) {
-     
     return <AuthStack />;
   }
-
-   
   return <AppStack />;
 };
 
